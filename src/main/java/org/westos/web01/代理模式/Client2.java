5815af48a674bd08d72cda0d8f8d70381d4f8625
liuchonghua <a397513182@qq.com>
2018-08-23 14:34:23 +0800

    动态代理

--- src/main/java/org/westos/web01/代理模式/Client2.java
+++ src/main/java/org/westos/web01/代理模式/Client2.java
@@ -4,12 +4,22 @@ import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 测试动态代理
+ */
 public class Client2 {
     public static void main(String[] args) {
+        //需要代理的类,类型必须是接口
         Subject subject=new RealSubject();
-        Subject proxySubject = (Subject) Proxy.newProxyInstance(Subject.class.getClassLoader(),
+        //获取一个代理类
+        Subject proxySubject = (Subject) Proxy.newProxyInstance(
+                //需要被代理的类的类加载器
+                Subject.class.getClassLoader(),
+                //需要被代理类的class
                 new Class[]{Subject.class},
+                //自己写的代理类InvocationHandler,里面是代理的方法
                 new ProxyHandler(subject));
+        //执行代理类中的方法
         proxySubject.method1();
 
 
